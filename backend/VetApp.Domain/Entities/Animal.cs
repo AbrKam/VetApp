8b6b0054741a7ef14d640b0da03d69f020a6f1e6
Kamil Abram <kamilabram67@gmail.com>
2025-12-17 19:32:39 +0100

    Added constructors and setters to Animal and Appointment classes

--- backend/VetApp.Domain/Entities/Animal.cs
+++ backend/VetApp.Domain/Entities/Animal.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using VetApp.Domain.Commons;
 using VetApp.Domain.Commons.Entities;
 using VetApp.Domain.Commons.Enums;
@@ -6,11 +7,32 @@ namespace VetApp.Domain.Entities
 {
     public class Animal: BaseEntity
     {
+        public Animal(){}
+        public Animal(Owner owner, string name, DateOnly birthDate, AnimalSpecies species, string breed, AnimalGender gender)
+        {
+            Owner = owner ?? throw new ArgumentNullException(nameof(owner));
+            Name = name;
+            BirthDate = birthDate;
+            Species = species;
+            Breed = breed;
+            Gender = gender;
+        }
+
         public string Name {get; private set;}
         public DateOnly BirthDate {get; private set;}
         public AnimalSpecies Species {get; private set;}
+        public string Breed {get; private set;}
         public AnimalGender Gender {get; private set;}
         public List<Appointment> LastAppointments {get; private set;} = new List<Appointment>();
         public Owner Owner {get; private set;}
+
+        public void SetName(string name) 
+            => Name = name;
+        public void SetBirthDate(DateOnly date)
+            => BirthDate = date;
+        public void SetBreed(string breed)
+            => Breed = breed;
+        public void SetGender(AnimalGender gender)
+            => Gender = gender;
     }
 }
--- backend/VetApp.Domain/Entities/Appointment.cs
+++ backend/VetApp.Domain/Entities/Appointment.cs
@@ -4,10 +4,42 @@ namespace VetApp.Domain.Commons.Entities
 {
     public class Appointment : BaseEntity
     {
+        public Appointment(){}
+
+        public Appointment(Animal animal, Veterinarian veterinarian, DateTime startAt, 
+            DateTime endAt, AppointmentPurpose appointmentPurpose)
+        {
+            Animal = animal;
+            Veterinarian = veterinarian;
+            AppointmentPurpose = appointmentPurpose;
+            StartAt = startAt;
+            EndAt = endAt;
+            AppointmentPurpose = appointmentPurpose;
+        }
+
         public Animal Animal {get; private set;}
         public Veterinarian Veterinarian {get; private set;}
-        public DateTime AppointmentDate {get; private set;}
+        public DateTime StartAt {get; private set;}
+        public DateTime EndAt {get; private set;}
         public AppointmentPurpose AppointmentPurpose {get; private set;}
         public string AppointmentSummary {get; private set;}
+
+        public void SetAnimal(Animal animal) 
+            => Animal = animal;
+        public void SetVeterinarian(Veterinarian veterinarian)
+            => Veterinarian = veterinarian;
+        public void SetAppointmentStartAtAndEndAt(DateTime startAt, DateTime endAt)
+        {
+            if (startAt >= endAt)
+            {
+                throw new ArgumentException("Appointment cannot end before it starts!");
+            }
+            StartAt = startAt;
+            EndAt = endAt;
+        }
+        public void SetAppointmentPurpose(AppointmentPurpose appointmentPurpose)
+            => AppointmentPurpose = appointmentPurpose;
+        public void SetAppointmentSummary(string appointmentSummary)
+            => AppointmentSummary = appointmentSummary;
     }
 }
